f8de6ba4384992635805fdc1c809f7e7b82c44db
tejashri1812 <121239869+tejashri1812@users.noreply.github.com>
2022-12-29 17:52:48 +0530

    initial commit

---
+++ teju3.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	int a,b,max;
+	printf("enter values for a and b:");
+	scanf("%d%d",&a,&b);
+	
+	max=a*b,a,b;
+	printf("larger of %d and %d is %d \n",a,b,max);
+	
+	getch();
+}
